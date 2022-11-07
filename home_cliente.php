e3394a07a834bff1853a2bd355c6058e3b06db87
willcrdo <willcrdo@hotmail.com>
2022-11-07 17:44:15 -0300

    alteracao nome site

--- home_cliente.php
+++ home_cliente.php
@@ -26,7 +26,7 @@ include('verifica_perfil_usr.php');
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="#page-top"><i class="fa-regular fa-scanner-keyboard"></i>Controle de estoque</a>
+                <a class="navbar-brand" href="#page-top"><i class="fa-regular fa-scanner-keyboard"></i>Controle de Clientes</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars ms-1"></i>
